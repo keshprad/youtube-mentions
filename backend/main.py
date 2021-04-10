5b44c2d6874052f5892eb59669d311f36ad16aad
keshprad <keshav.r.pradeep@gmail.com>
2021-04-09 17:42:44 -0700

    Started FastAPI app

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+origins = ["http://localhost:5000"]
+app.add_middleware(CORSMiddleware, allow_origins=origins,
+                   allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
+
+@app.get("/")
+def root():
+    return {"Hello": "world"}
