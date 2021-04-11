264758af2317e6e108da19515742bb6f07394c52
Daniel <danieltomelee64@gmail.com>
2021-04-10 19:22:52 -0700

    Fix error handling if video does not allow a transcript

--- backend/card_generators/entity.py
+++ backend/card_generators/entity.py
@@ -12,7 +12,11 @@ import wikipedia
 #     return None
 
 async def identify_entities(video_id: str) -> List:
-    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    try:    
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    except Exception as e:
+        print(e)
+        return []
 
     lines = []
 
